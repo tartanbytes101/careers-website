9cb77ba2a2042f2a5cb17e3ce7e7b0cc0f836259
tartanbytes101 <131682609+tartanbytes101@users.noreply.github.com>
2023-04-30 23:13:10 +0000

    added question set

---
+++ questions.py
@@ -0,0 +1,100 @@
+QUESTIONS = [{
+  'id': 1,
+  'question':
+  'Which of the following is determined during the analysis phase of software development?',
+  'answer_1': 'Fitness for purpose',
+  'answer_2': 'Functional requirements',
+  'answer_3': 'Pseudocode',
+  'answer_4': 'Error Handling',
+  'correct': 'answer_2'
+}, {
+  'id': 2,
+  'question': 'Which of the following is a definition of a program input?',
+  'answer_1': 'Data that will be entered by a user or received from a file',
+  'answer_2':
+  'Data that is to be initialised by the programmer during implementation and that will not change when the software is in use',
+  'answer_3': 'Results of testing',
+  'answer_4': 'Actual Parameters',
+  'correct': 'answer_1'
+}, {
+  'id': 3,
+  'question': 'Which of the following is a definition of a process?',
+  'answer_1':
+  'Code created to execute a sequence of instructions that may make use of inputs and may result in output to the user',
+  'answer_2': 'Data received from a text file',
+  'answer_3': 'Data sent to a printer',
+  'answer_4': 'Syntax Error feedback',
+  'correct': 'answer_1'
+}, {
+  'id': 4,
+  'question': 'Which of the following is a definition of a program output?',
+  'answer_1': 'Data that is validated after being entered by a user',
+  'answer_2':
+  'Data that is sent from the program to the user, a file or peripheral',
+  'answer_3': 'Selection statements',
+  'answer_4': 'Syntax Error feedback',
+  'correct': 'answer_2'
+}, {
+  'id': 5,
+  'question':
+  'What would you consider when outlining assumptions about new software that is being developed?',
+  'answer_1': 'Hardware needed to run the software',
+  'answer_2': 'Data structures',
+  'answer_3': 'Normal data',
+  'answer_4': 'Character Encoding',
+  'correct': 'answer_1'
+}, {
+  'id': 6,
+  'question':
+  'Why is it important to identify appropriate project boundaries during the software analysis phase?',
+  'answer_1': 'So that what is entered into the program will work properly',
+  'answer_2': 'So that a program doesn’t use too much memory',
+  'answer_3': 'So that it is sure what it is doing',
+  'answer_4':
+  'So that the client cannot add new functionality requirements during the project development without incurring additional cost implications',
+  'correct': 'answer_4'
+}, {
+  'id': 7,
+  'question':
+  'Which of the following statements best describes the Purpose of a project during the analysis stage',
+  'answer_1':
+  'A list of what will be delivered to the client at the end of the project',
+  'answer_2': 'A list of what will and will not be included in the project',
+  'answer_3': 'A definition of the projects inputs, processes and outputs',
+  'answer_4':
+  'The reason for the project including the client or end user needs',
+  'correct': 'answer_4'
+}, {
+  'id': 8,
+  'question':
+  'Which of the following statements best describes the Functional Requirements of a project during the analysis stage',
+  'answer_1':
+  'A list of what will be delivered to the client at the end of the project',
+  'answer_2': 'A list of what will and will not be included in the project',
+  'answer_3': 'A definition of the projects inputs, processes and outputs',
+  'answer_4':
+  'The reason for the project including the client or end user needs',
+  'correct': 'answer_3'
+}, {
+  'id': 9,
+  'question':
+  'Which of the following statements best describes the Scope of a project during the analysis stage',
+  'answer_1':
+  'A list of what will be delivered to the client at the end of the project',
+  'answer_2': 'A list of what will and will not be included in the project',
+  'answer_3': 'A definition of the projects inputs, processes and outputs',
+  'answer_4':
+  'The reason for the project including the client or end user needs',
+  'correct': 'answer_1'
+}, {
+  'id': 10,
+  'question':
+  'Which of the following statements best describes the Boundary of a project during the analysis stage',
+  'answer_1':
+  'A list of what will be delivered to the client at the end of the project',
+  'answer_2': 'A list of what will and will not be included in the project',
+  'answer_3': 'A definition of the projects inputs, processes and outputs',
+  'answer_4':
+  'The reason for the project including the client or end user needs',
+  'correct': 'answer_2'
+}]
